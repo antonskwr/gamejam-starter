2b0981a38de468cd7f9e7aa04838761c15e0adbd
Anton Skvartsou <antonskwr@gmail.com>
2020-07-11 16:47:48 +0200

    Added basic player movement

---
+++ godot/SCENE/Entity/Player/Player.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 70
+const FRICTION = 0.2
+
+var motion = Vector2()
+
+func _physics_process(delta) -> void:
+	update_motion(delta)
+	move_and_slide(self.motion)
+
+func update_motion(delta) -> void:
+	if Input.is_action_pressed("move_up") and not Input.is_action_pressed("move_down"):
+		self.motion.y = -MAX_SPEED #add lerp for better feel
+		# self.animation_direction.y = -1
+	elif not Input.is_action_pressed("move_up") and Input.is_action_pressed("move_down"):
+		self.motion.y = MAX_SPEED # add lerp for better feel
+		# self.animation_direction.y = 1
+	else:
+		self.motion.y = lerp(motion.y, 0, FRICTION)
+		# self.animation_direction.y = 0
+
+	if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		self.motion.x = -MAX_SPEED #add lerp for better feel
+		# self.animation_direction.x = -1
+	elif not Input.is_action_pressed("move_left") and Input.is_action_pressed("move_right"):
+		self.motion.x = MAX_SPEED #add lerp for better feel
+		# self.animation_direction.x = 1
+	else:
+		self.motion.x = lerp(motion.x, 0, FRICTION)
+		# self.animation_direction.x = 0
+
+	self.motion = self.motion.clamped(MAX_SPEED)
+
+
+
